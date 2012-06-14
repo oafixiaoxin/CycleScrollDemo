d8c349696578395652efa29283f10f175b6acf5c
Linkcity <macbookpro@linkcity.local>
2012-06-14 17:32:39 +0800

    Initial Commit

---
+++ CycleScrollDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CycleScrollDemo
+//
+//  Created by Weever Lu on 12-6-14.
+//  Copyright (c) 2012年 linkcity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CycleScrollDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  CycleScrollDemo
+//
+//  Created by Weever Lu on 12-6-14.
+//  Copyright (c) 2012年 linkcity. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
